d4eb3b4d16392ae074a79f24f72edc0c46e610aa
Hampus Nilsson <hampus.nilsson@kalmarenergi.se>
2023-07-06 09:29:50 +0200

    test

--- install.sh
+++ install.sh
@@ -24,6 +24,8 @@ else
     echo "The line already exists in .zshrc. Skipping..."
 fi
 
+echo "alias reloaddot='$DEST_DIR/install.sh'" >> $DEST_DIR/reload
+
 echo "Reloading .zshrc..."
 zsh -i -c "source $HOME/.zshrc"
 
