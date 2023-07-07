0cfdb7c9353f7783861cccbadeb9f1a63361324a
wachtelhund <hampus.nilsson222@hotmail.se>
2023-07-07 17:44:15 +0200

    only push dots on dotpush

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+DEST_DIR=$(find $HOME -type d -name "dotfiles")
 
 read -p "Are you sure to want to push to the remote repository? (y/n) " REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Pushing to remote repository..."
-    git add -A
+    git add $DEST_DIR/dots
     git commit -m "Update dotfiles"
     git push origin main
 else
