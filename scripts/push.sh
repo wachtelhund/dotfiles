9cdec732ed0019fa48ac0ba0bb8ae08b893232f9
wachtelhund <hampus.nilsson222@hotmail.se>
2023-07-07 17:40:05 +0200

    Update dotfiles

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,10 +1,12 @@
-read -p "Are you sure to want to push to the remote repository? (y/n) " -n 1 -r REPLY
+#!/bin/bash
+
+read -p "Are you sure to want to push to the remote repository? (y/n) " REPLY
 
 if [[ $REPLY =~ ^[Yy]$ ]]; then
     echo "Pushing to remote repository..."
     git add -A
     git commit -m "Update dotfiles"
-    git push origin master
+    git push origin main
 else
     echo "Aborting..."
 fi
